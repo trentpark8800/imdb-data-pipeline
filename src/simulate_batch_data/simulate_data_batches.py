9bdd78f6298afd97439a49f0c3264457b2e86631
Trent Park <trentpark8800@gmail.com>
2021-05-27 18:09:03 +0200

    Add skeleton code for the data batching task

---
+++ src/simulate_batch_data/simulate_data_batches.py
@@ -0,0 +1,19 @@
+"""
+@purpose: Main script to simulate the time-driven arrival of json data
+from static data provided.
+"""
+import json
+import pandas as pd
+
+
+def main():
+    """
+    Main entry point for the script.
+    """
+    # Read in static file from some location
+
+    # Write a specific number of lines to some location
+
+
+if __name__ == '__main__':
+    main()
